bd63a229e3917ff5876f627bedd0058e6c2ce55a
FenFr <HohesCAlt@gmail.com>
2022-11-30 23:01:23 +0100

    stuff

--- Uebung_11-QT-Tutorial_2/T2.pro
+++ Uebung_11-QT-Tutorial_2/T2.pro
@@ -2,12 +2,12 @@ TEMPLATE	= app
 CONFIG     += qt warn_on
 HEADERS	    =
 SOURCES	    = main.cpp
-TARGET		= T1
+TARGET		= T2
 QTDIR_build:REQUIRES="contains(QT_CONFIG, small-config)"
 
 # install
-target.path     = Uebung_10-QT-Tutorial_1/T1
-sources.files   = $$SOURCES $$HEADERS $$RESOURCES $$FORMS T1.pro
-sources.path    = Uebung_10-QT-Tutorial_1/T1
+target.path     = Uebung_11-QT-Tutorial_2/T2
+sources.files   = $$SOURCES $$HEADERS $$RESOURCES $$FORMS T2.pro
+sources.path    = Uebung_11-QT-Tutorial_2/T2
 INSTALLS       += target sources
 QT             += widgets
---
+++ Uebung_11-QT-Tutorial_2/main.cpp
@@ -0,0 +1,28 @@
+//////////////////////////////////////////////////////////////////////////////
+// Course:	  			C++ / GUI
+// Lecturer:      		Prof. Dr.-Ing. habil. Ralf Salomon
+// Exercise instructor: M.Sc. Theo Gabloffsky
+// Author:              Fenya Freitag
+// Exercise:      		11
+// Task:          		1
+// Name:          		main.cpp
+// Description:   		
+//////////////////////////////////////////////////////////////////////////////
+
+
+#include <QApplication>
+#include <QPushButton>
+#include <QWidget>
+#include <QFont>
+
+
+// MAIN //////////////////////////////////////////////////////////////////////
+
+int main(int argc, char**argv) {
+
+    QApplication app(argc, argv);
+
+}
+
+
+// MAIN END //////////////////////////////////////////////////////////////////
