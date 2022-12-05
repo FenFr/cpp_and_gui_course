883ae6a8d1bece900d08ef550b5f16ef5ee97a8e
FenFr <HohesCAlt@gmail.com>
2022-12-05 22:32:40 +0100

    Stuff

--- Uebung_11-QT-Tutorial_2/Task_2/main.cpp
+++ Uebung_11-QT-Tutorial_2/Task_2/main.cpp
@@ -17,6 +17,7 @@
 #include <QSlider>
 #include <QVBoxLayout>
 #include <QWidget>
+#include <QSpinBox>
 
 
 class MyWidget : public QWidget {
@@ -31,20 +32,25 @@ MyWidget::MyWidget(QWidget *parent)
     QPushButton *quit = new QPushButton(tr("Quit"));
     quit->setFont(QFont("Times", 18, QFont::Bold));
 
-    QLCDNumber *lcd = new QLCDNumber(2);
+    QLCDNumber *lcd = new QLCDNumber(3);
     lcd->setSegmentStyle(QLCDNumber::Filled);
 
     QSlider *slider = new QSlider(Qt::Horizontal);
-    slider->setRange(0, 99);
+    slider->setRange(0, 420);
     slider->setValue(0);
 
+    QSpinBox *spinner = new QSpinBox();
+    spinner->setRange(0, 420);
+
     connect(quit, SIGNAL(clicked()), qApp, SLOT(quit()));
-    connect(slider, SIGNAL(valueChanged(int)), lcd, SLOT(display(int)));
+    connect(slider,  SIGNAL(valueChanged(int)), lcd, SLOT(display(int)));
+    connect(spinner, SIGNAL(valueChanged(int)), lcd, SLOT(display(int)));
 
     QVBoxLayout *layout = new QVBoxLayout;
     layout->addWidget(quit);
     layout->addWidget(lcd);
     layout->addWidget(slider);
+    layout->addWidget(spinner);
     setLayout(layout);
 }
 
